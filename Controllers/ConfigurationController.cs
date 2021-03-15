56ffd8ae773e861926205c5afe544710e0f7daea
Nabeel Sulieman <nabeel.sulieman@live.com>
2021-03-15 09:32:32 -0700

    - Remove the uses of "op" in favor of separate controller actions - Split out home controller into separate smaller controllers

---
+++ Controllers/ConfigurationController.cs
@@ -0,0 +1,74 @@
+﻿using KCert.Models;
+using KCert.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KCert.Controllers
+{
+    [Route("configuration")]
+    public class ConfigurationController : Controller
+    {
+        private readonly KCertClient _kcert;
+        private readonly EmailClient _email;
+        private readonly CertClient _cert;
+        private readonly ILogger<ConfigurationController> _log;
+
+        public ConfigurationController(KCertClient kcert, EmailClient email, CertClient cert, ILogger<ConfigurationController> log)
+        {
+            _kcert = kcert;
+            _email = email;
+            _cert = cert;
+            _log = log;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> IndexAsync(bool sendEmail = false)
+        {
+            var p = await _kcert.GetConfigAsync();
+            return View(p ?? new KCertParams());
+        }
+
+        [HttpGet("test-email")]
+        public async Task<IActionResult> TestEmailAsync()
+        {
+            var p = await _kcert.GetConfigAsync();
+            await _email.SendTestEmailAsync(p);
+            return RedirectToAction("Index");
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> SaveAsync([FromForm] ConfigurationForm form)
+        {
+            var p = await _kcert.GetConfigAsync() ?? new KCertParams();
+
+            p.AcmeDirUrl = new Uri(form.AcmeDir);
+            p.AcmeEmail = form.AcmeEmail;
+            p.EnableAutoRenew = form.EnableAutoRenew;
+            p.SmtpHost = form.SmtpHost;
+            p.SmtpPort = form.SmtpPort;
+            p.SmtpUser = form.SmtpUser;
+            p.SmtpPass = form.SmtpPass;
+            p.EmailFrom = form.EmailFrom;
+            p.TermsAccepted = form.TermsAccepted;
+
+            if (!(form?.AcmeKey?.All(c => c == '*') ?? false))
+            {
+                _log.LogInformation("Setting key value from form");
+                p.AcmeKey = form.AcmeKey;
+            }
+
+            if (string.IsNullOrWhiteSpace(p.AcmeKey))
+            {
+                _log.LogInformation("Generating new key");
+                p.AcmeKey = _cert.GenerateNewKey();
+            }
+
+            await _kcert.SaveConfigAsync(p);
+            return RedirectToAction("Index");
+        }
+    }
+}
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,11 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using KCert.Models;
 using System.Threading.Tasks;
-using System.Linq;
-using Microsoft.Extensions.Logging;
-using System;
 using KCert.Services;
-using System.Text.Json;
 
 namespace KCert.Controllers
 {
@@ -15,155 +11,24 @@ namespace KCert.Controllers
     {
         private readonly KCertClient _kcert;
         private readonly K8sClient _kube;
-        private readonly CertClient _cert;
-        private readonly EmailClient _email;
-        private readonly KCertConfig _cfg;
-        private readonly ILogger<HomeController> _log;
 
-        public HomeController(KCertClient kcert, K8sClient kube, ILogger<HomeController> log, CertClient cert, EmailClient email, KCertConfig cfg)
+        public HomeController(KCertClient kcert, K8sClient kube)
         {
             _kcert = kcert;
             _kube = kube;
-            _log = log;
-            _cert = cert;
-            _email = email;
-            _cfg = cfg;
         }
 
         [HttpGet]
-        public async Task<IActionResult> IndexAsync(string op, string ns, string name)
+        public async Task<IActionResult> IndexAsync()
         {
-            if (op == "renew")
-            {
-                await _kcert.RenewCertAsync(ns, name);
-                return RedirectToAction("Index");
-            }
-
             var secrets = await _kube.GetManagedSecretsAsync();
             return View(secrets);
         }
 
-        [HttpGet("new")]
-        public IActionResult NewCertAsync() => View("EditCert");
-
-        [HttpPost("new")]
-        public async Task<IActionResult> CreateCertAsync(string ns, string name, string[] hosts)
+        [HttpGet("renew/{ns}/{name}")]
+        public async Task<IActionResult> RenewAsync(string ns, string name)
         {
-            _log.LogInformation(JsonSerializer.Serialize(new { ns, name, hosts }));
-            await _kcert.RenewCertAsync(ns, name, hosts);
-            return RedirectToAction("Index");
-        }
-
-        [HttpGet("edit/{ns}/{name}")]
-        public async Task<IActionResult> EditCertAsync(string ns, string name, string op)
-        {
-            if (op == "unmanage")
-            {
-                await _kube.UnmanageSecretAsync(ns, name);
-                return RedirectToAction("Index");
-            }
-
-            if (op == "delete")
-            {
-                await _kube.DeleteSecretAsync(ns, name);
-                return RedirectToAction("Index");
-            }
-            
-            var cert = await _kube.GetSecretAsync(ns, name);
-            return View(cert);
-        }
-
-        [HttpPost("edit/{ns}/{name}")]
-        public async Task<IActionResult> UpdateCertAsync(string ns, string name, string[] hosts)
-        {
-            var secret = await _kube.GetSecretAsync(ns, name);
-            var cert = _cert.GetCert(secret);
-            var currentHosts = _cert.GetHosts(cert);
-            
-            // If there's a change, renew the cert
-            if (hosts.Length != currentHosts.Count || hosts.Intersect(currentHosts).Count() != hosts.Length)
-            {
-                await _kcert.RenewCertAsync(ns, name, hosts);
-            }
-
-            return RedirectToAction("Index");
-        }
-
-        [HttpGet("challenge")]
-        public async Task<IActionResult> ChallengeIngressAsync(string op)
-        {
-            if (op == "refresh")
-            {
-                await _kcert.SyncHostsAsync();
-                return RedirectToAction();
-            }
-
-            var ingress = await _kube.GetIngressAsync(_cfg.KCertNamespace, _cfg.KCertIngressName);
-            return View(ingress);
-        }
-
-        [HttpGet("manage")]
-        public async Task<IActionResult> ManageSecretsAsync(string op, string ns, string name)
-        {
-            if (op == "manage")
-            {
-                await _kube.ManageSecretAsync(ns, name);
-                return RedirectToAction();
-            }
-
-            var secrets = await _kube.GetUnmanagedSecretsAsync();
-            return View(secrets);
-        }
-
-        [HttpGet("configuration")]
-        public async Task<IActionResult> ConfigurationAsync(bool sendEmail = false)
-        {
-            var p = await _kcert.GetConfigAsync();
-
-            if (sendEmail)
-            {
-                await _email.SendTestEmailAsync(p);
-                return RedirectToAction("Configuration");
-            }
-
-            return View(p ?? new KCertParams());
-        }
-
-        [HttpPost("configuration")]
-        public async Task<IActionResult> SaveConfigAsync([FromForm] ConfigurationForm form)
-        {
-            var p = await _kcert.GetConfigAsync() ?? new KCertParams();
-
-            p.AcmeDirUrl = new Uri(form.AcmeDir);
-            p.AcmeEmail = form.AcmeEmail;
-            p.EnableAutoRenew = form.EnableAutoRenew;
-            p.SmtpHost = form.SmtpHost;
-            p.SmtpPort = form.SmtpPort;
-            p.SmtpUser = form.SmtpUser;
-            p.SmtpPass = form.SmtpPass;
-            p.EmailFrom = form.EmailFrom;
-            p.TermsAccepted = form.TermsAccepted;
-
-            if (!(form?.AcmeKey?.All(c => c == '*') ?? false))
-            {
-                _log.LogInformation("Setting key value from form");
-                p.AcmeKey = form.AcmeKey;
-            }
-
-            if (string.IsNullOrWhiteSpace(p.AcmeKey))
-            {
-                _log.LogInformation("Generating new key");
-                p.AcmeKey = _cert.GenerateNewKey();
-            }
-
-            await _kcert.SaveConfigAsync(p);
-            return RedirectToAction("Configuration");
-        }
-
-        [HttpPost("challenge")]
-        public async Task<IActionResult> SyncChallengeHostsAsync()
-        {
-            await _kcert.SyncHostsAsync();
+            await _kcert.RenewCertAsync(ns, name);
             return RedirectToAction("Index");
         }
 
