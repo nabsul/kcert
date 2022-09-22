40f7efafd4e626144da5ede6ec3484e6ededb6dd
Nabeel Sulieman <n@runto.co>
2022-09-21 20:08:35 -0700

    Further simplify

--- Program.cs
+++ Program.cs
@@ -2,13 +2,13 @@ using KCert;
 using KCert.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Linq;
 using System.Reflection;
 
+// command line option for manually generating an ECDSA key
 if (args.Length > 0 && args[^1] == "generate-key")
 {
     Console.WriteLine("Generating ACME Key");
@@ -19,24 +19,19 @@ if (args.Length > 0 && args[^1] == "generate-key")
 
 var builder = WebApplication.CreateBuilder(args);
 
-var serviceTypes = Assembly.GetExecutingAssembly().GetTypes()
-    .Where(t => t.GetCustomAttribute<ServiceAttribute>() != null);
-
-foreach (var t in serviceTypes)
-{
-    builder.Services.AddSingleton(t);
-}
+// Add all services marked with the [Service] attribute
+Assembly.GetExecutingAssembly().GetTypes()
+    .Where(t => t.GetCustomAttribute<ServiceAttribute>() != null)
+    .ToList().ForEach(t => builder.Services.AddSingleton(t));
 
 builder.Services.AddControllersWithViews();
-
-builder.Configuration.AddUserSecrets<Program>(optional: true);
-builder.Configuration.AddEnvironmentVariables();
 
 builder.WebHost.ConfigureKestrel(opt => {
     opt.ListenAnyIP(80);
     opt.ListenAnyIP(8080);
 });
 
+// add background services
 builder.Services.AddHostedService<RenewalService>();
 builder.Services.AddHostedService<IngressMonitorService>();
 builder.Services.AddHostedService<ConfigMonitorService>();
