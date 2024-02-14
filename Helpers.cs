b1219ce97d0e96b5fd276eca3d88bbd8bee291c2
Nabeel Sulieman <nabsul@users.noreply.github.com>
2024-02-14 15:14:59 -0800

    eof

--- Helpers.cs
+++ Helpers.cs
@@ -19,4 +19,11 @@ public static class Helpers
         }
         while (tok != null);
     }
+
+    public static async Task<List<T>> ToListAsync<T>(this IAsyncEnumerable<T> enumerable)
+    {
+        var list = new List<T>();
+        await foreach (var item in enumerable) list.Add(item);
+        return list;
+    }
 }
